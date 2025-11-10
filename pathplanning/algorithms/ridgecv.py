efee11633a8d19143548559c43ff949122fa92d3
Paul Swoboda <paul.swoboda@trumpf.com>
2025-11-10 15:38:43 +0100

    changed ridge to ridgecv for more robustness

--- pathplanning/algorithms/ridgecv.py
+++ pathplanning/algorithms/ridgecv.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import RidgeCV
 from sklearn.preprocessing import PolynomialFeatures
 
 
@@ -20,10 +20,10 @@ def ridge(lane):
     poly = PolynomialFeatures(degree)
     x_poly = poly.fit_transform(x.reshape(-1, 1))
 
-    reg = Ridge().fit(x_poly, y)
+    reg = RidgeCV().fit(x_poly, y)
 
     # Sample points along the x-range for visualization
-    x_vis = np.linspace(min(x.min(), x.max()), max(x.min(), x.max()), 500)
+    x_vis = np.linspace(x.min(), x.max(), 100)
     x_vis_poly = poly.transform(x_vis.reshape(-1, 1))
     y_vis = reg.predict(x_vis_poly)
     points = np.column_stack((x_vis, y_vis)).tolist()
