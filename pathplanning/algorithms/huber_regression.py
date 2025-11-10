e7d5f19e6612825eb6cfe31d012ee41790f18a82
Paul Swoboda <paul.swoboda@trumpf.com>
2025-11-10 14:37:33 +0100

    added publishers for debug lanes; added ridge and huber_regression

---
+++ pathplanning/algorithms/huber_regression.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn.linear_model import HuberRegressor
+from sklearn.preprocessing import PolynomialFeatures
+
+
+def huber_regression(lane):
+    """
+    Huber Regression algorithm.
+
+    Args:
+        lane (dict): Detected lane information.
+
+    Returns:
+        list: List of points representing the fitted lane.
+    """
+    x = np.array([point[0] for point in lane["points"]])
+    y = np.array([point[1] for point in lane["points"]])
+
+    degree = 3
+    poly = PolynomialFeatures(degree)
+    x_poly = poly.fit_transform(x.reshape(-1, 1))
+
+    reg = HuberRegressor().fit(x_poly, y)
+
+    # Sample points along the x-range for visualization
+    x_vis = np.linspace(min(x.min(), x.max()), max(x.min(), x.max()), 500)
+    x_vis_poly = poly.transform(x_vis.reshape(-1, 1))
+    y_vis = reg.predict(x_vis_poly)
+    points = np.column_stack((x_vis, y_vis)).tolist()
+
+    return points
--- pathplanning/path_planning_node.py
+++ pathplanning/path_planning_node.py
@@ -1,16 +1,15 @@
 # Copyright (c) 2025 Smart Rollerz e.V.
 # All rights reserved.
 
-import geometry_msgs.msg
 import numpy as np
-from camera_preprocessing.transformation.birds_eyed_view import Birdseye
-from camera_preprocessing.transformation.coordinate_transform import CoordinateTransform
-from camera_preprocessing.transformation.distortion import Distortion
+import rclpy
+import visualization_msgs
+from geometry_msgs.msg import Point
 from lane_msgs.msg import Lane, LaneDetectionResult
-from smarty_utils.enums import Location, NodeState
 from smarty_utils.smarty_node import SmartyNode
+from visualization_msgs.msg import Marker
 
-from pathplanning.framework.pathplanningController import PPController
+from pathplanning.algorithms import huber_regression, ridge
 
 
 def serialize_lane(Lane: Lane):
@@ -25,7 +24,7 @@ def serialize_lane(Lane: Lane):
         dict -- Serialized lane information.
     """
     return {
-        "points": [[point.x, point.y, point.z] for point in Lane.points],
+        "points": [[point.x / 1000, point.y / 1000] for point in Lane.points],
         "detected": Lane.detected,
     }
 
@@ -38,15 +37,106 @@ class PathPlanningNode(SmartyNode):
         super().__init__(
             "path_planning_node",
             "pathplanning",
-            subscribed_topics={
-                "/lane_detection/lane": (
-                    LaneDetectionResult,
-                    self.serialized_points,
-                    None,
-                )
-            },
         )
 
-        self.left_path_publisher = self.create_publisher(
-            geometry_msgs.msg.Vector3, "/path_planning/target/left", 10
+        self.lane_detection_subscription = self.create_subscription(
+            LaneDetectionResult,
+            "/lane_detection/lane",
+            self.receive_lane_detection_result,
+            10,
         )
+
+        self.left_debug_publisher = self.create_publisher(
+            visualization_msgs.msg.Marker, "/path_planning/debug/left", 10
+        )
+        self.center_debug_publisher = self.create_publisher(
+            visualization_msgs.msg.Marker, "/path_planning/debug/center", 10
+        )
+        self.right_debug_publisher = self.create_publisher(
+            visualization_msgs.msg.Marker, "/path_planning/debug/right", 10
+        )
+
+        # Store previous regression coefficients for each lane
+        self.prev_coeffs = {
+            "left": None,
+            "center": None,
+            "right": None,
+        }
+
+    def receive_lane_detection_result(self, result: LaneDetectionResult):
+        """
+        Process serialized lane points.
+
+        Arguments:
+            result -- Lane detection result message.
+        """
+        lanes = [
+            ("left", self.left_debug_publisher, (255, 0, 0)),
+            ("center", self.center_debug_publisher, (0, 255, 0)),
+            ("right", self.right_debug_publisher, (0, 0, 255)),
+        ]
+
+        for lane_name, publisher, color in lanes:
+            lane = getattr(result, lane_name)
+            serialized_lane = serialize_lane(lane)
+
+            if len(serialized_lane["points"]) < 10 or not lane.detected:
+                continue
+
+            points = huber_regression.huber_regression(serialized_lane)
+
+            self.publish_list_of_points(points, publisher, color)
+
+    def publish_list_of_points(self, points, publisher, color=(1.0, 1.0, 1.0)):
+        """
+        Publishes a list of points as a line strip marker.
+
+        Arguments:
+            points -- List of points to publish. Format: [[x1, y1], [x2, y2], ...].
+            publisher -- ROS publisher to use.
+            color -- Tuple representing the RGB color of the line (going from 0 - 255).
+        """
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.header.stamp = self.get_clock().now().to_msg()
+        marker.ns = "lane_polynom"
+        marker.id = 0
+        marker.type = Marker.LINE_STRIP
+        marker.action = Marker.ADD
+        marker.scale.x = 0.03  # Line width
+        marker.color.a = 1.0
+        marker.color.r = color[0] / 255
+        marker.color.g = color[1] / 255
+        marker.color.b = color[2] / 255
+        marker.pose.orientation.w = 1.0
+
+        for point in points:
+            p = Point()
+            p.x = point[0]
+            p.y = point[1]
+            p.z = 0.0
+            marker.points.append(p)
+        publisher.publish(marker)
+
+
+def main(args=None):
+    """
+    Main function to start the path planning node.
+
+    Keyword Arguments:
+        args -- Arguments for the Node (default: {None})
+    """
+    rclpy.init(args=args)
+    node = PathPlanningNode()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
