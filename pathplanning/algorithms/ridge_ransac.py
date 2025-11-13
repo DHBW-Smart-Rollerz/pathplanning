c4a6a1e00865286adafbf486f6ab72071dfd3bfb
Paul Swoboda <paul.swoboda@trumpf.com>
2025-11-13 12:31:18 +0100

    added ridge_ransac algorithm

---
+++ pathplanning/algorithms/ridge_ransac.py
@@ -0,0 +1,32 @@
+import numpy as np
+from sklearn.linear_model import RANSACRegressor, RidgeCV
+from sklearn.preprocessing import PolynomialFeatures
+
+
+def ridge_ransac(lane):
+    """
+    Ridge Regression algorithm.
+
+    Args:
+        lane (dict): Detected lane information.
+
+    Returns:
+        list: List of points representing the fitted lane.
+    """
+    x = np.array([point[0] for point in lane["points"]])
+    y = np.array([point[1] for point in lane["points"]])
+
+    degree = 3
+    poly = PolynomialFeatures(degree)
+    x_poly = poly.fit_transform(x.reshape(-1, 1))
+
+    base = RidgeCV()
+    reg = RANSACRegressor(base, residual_threshold=0.1, min_samples=5).fit(x_poly, y)
+
+    # Sample points along the x-range for visualization
+    x_vis = np.linspace(x.min(), x.max(), 100)
+    x_vis_poly = poly.transform(x_vis.reshape(-1, 1))
+    y_vis = reg.predict(x_vis_poly)
+    points = np.column_stack((x_vis, y_vis)).tolist()
+
+    return points
